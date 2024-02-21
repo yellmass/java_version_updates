f98157bdd6973542b45d2c4cc2b8786689503412
yellmass <ebincyilmaz@gmail.com>
2024-02-20 20:00:19 -0500

    generics

--- src/main/java/com/cydeo/Main.java
+++ src/main/java/com/cydeo/Main.java
@@ -1,7 +1,26 @@
 package com.cydeo;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> items = new ArrayList<>();
+
+        items.add(2);
+        items.add(3);
+        items.add(4);
+        items.add(5);
+
+        printDoubled(items);
+
+
+    }
+
+    private static void printDoubled(ArrayList<Integer> items) {
+        for(int item : items){
+            System.out.println(item*2);
+        }
+
+
     }
 }
